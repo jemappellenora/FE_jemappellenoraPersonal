29b47ef93829f28904ef2c22137e8c6ed4bd6f07
Ching(Nora) Lau <chingwlau@gmail.com>
2020-12-28 18:20:52 -0500

    Style: experiences card

--- src/components/views/ExperiencesView.jsx
+++ src/components/views/ExperiencesView.jsx
@@ -4,52 +4,52 @@ function ExperiencesView() {
   return (
     <div id="dummyExperiences">
       <h3>Experiences</h3>
-      <div>
+      <div className="expCard">
         <h4>Commit to Green | JUNE2020 - AUG2020</h4>
         <h6>Front-End Software Development Intern</h6>
-        <ul>
+        <li>
           Implemented frontend application using React-Redux.js to maintain
           hierarchical and intuitive front-end structure
-        </ul>
-        <ul>
+        </li>
+        <li>
           Improved interactions with Google Maps API to display and store
           additional relevant data
-        </ul>
+        </li>
       </div>
-      <div>
+      <div className="expCard">
         <h4>NYC Tech Talent Pipeline | MAY2020 - JUNE2020</h4>
         <h6>Full-Stack Software Development Intern</h6>
-        <ul>
+        <li>
           Created a full-stack CRUD application using Sequelize ORM with Node.js
           and Express.js, for backend server, and React-redux.js, for frontend
           client application
-        </ul>
-        <ul>
+        </li>
+        <li>
           Used CSS, HTML, jQuery and JavaScript to enhance user experience and
           interface design
-        </ul>
+        </li>
       </div>
-      <div>
+      <div className="expCard">
         <h4>J.P.Morgan | April2020</h4>
         <h6>Virtual Software Engineer Intern</h6>
-        <ul>
+        <li>
           Utilized understanding of Python scripting language, Command line
           basics, Javascript, React, Typescript and J.P.Morgan Perspective tool
           to manipulate and present data feeds visually in web applications
-        </ul>
+        </li>
       </div>
-      <div>
+      <div className="expCard">
         <h4>WITNY Summer Guild at Cornell Tech | June2018</h4>
         <h6>Participant</h6>
-        <ul>
+        <li>
           Collaborated with team members to create a product design prototype
           and designed a User-centered Application using HTML, Javascript and
           CSS
-        </ul>
-        <ul>
+        </li>
+        <li>
           Analyzed qualitative data to identity problems or possible
           improvements of the application
-        </ul>
+        </li>
       </div>
     </div>
   );
