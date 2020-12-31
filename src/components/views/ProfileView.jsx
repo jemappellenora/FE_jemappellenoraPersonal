907513cc79e696a1f774fe219f0f682cdda221ec
Ching(Nora) Lau <chingwlau@gmail.com>
2020-12-31 17:14:08 -0500

    Style: updated to table format

--- src/components/views/ProfileView.jsx
+++ src/components/views/ProfileView.jsx
@@ -6,13 +6,27 @@ function ProfileView() {
     <div>
       <h3>Jemappellenora</h3>
       <img src={headShot} style={{ height: "500px" }}></img>
-      <table className="genInfo">
+      <table className="genInfoTable">
         <tbody>
-          Education: Bachelor's degree of Computer Science and Information
-          Security
+          <tr>
+            <td>Education:</td>
+            <td>
+              Bachelor's degree of Computer Science and Information Security
+            </td>
+          </tr>
         </tbody>
-        <tbody>Commulative GPA: 3.5</tbody>
-        <tbody>Major GPA: 3.7</tbody>
+        <tbody>
+          <tr>
+            <td>Cumulative GPA: </td>
+            <td>3.5</td>
+          </tr>
+        </tbody>
+        <tbody>
+          <tr>
+            <td>Major GPA:</td>
+            <td> 3.7</td>
+          </tr>
+        </tbody>
       </table>
     </div>
   );
