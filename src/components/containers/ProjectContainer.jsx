df1570fdd3a7c7be15a12683041ef14e6d53dc08
jemappellenora <chingwlau@gmail.com>
2021-01-03 20:48:13 -0500

    Update: Resume on site

--- src/components/containers/ProjectContainer.jsx
+++ src/components/containers/ProjectContainer.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import { ProjectView } from "../views";
+import { Ching_Wing_Nora_Lau_Resume } from "../images";
 
 class ProjectContainer extends Component {
   render() {
     return (
       <div>
+        <img alt="resume" source={Ching_Wing_Nora_Lau_Resume}/>
         <ProjectView />
       </div>
     );
