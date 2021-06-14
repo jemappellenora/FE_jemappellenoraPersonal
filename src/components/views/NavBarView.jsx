7d784df18fae2ef9d0aa382114c612a2ce18b2d6
jemappellenora <chingwlau@gmail.com>
2021-06-13 22:22:39 -0400

    update: add alt for images

--- src/components/views/NavBarView.jsx
+++ src/components/views/NavBarView.jsx
@@ -6,19 +6,19 @@ const NavBarView = () => {
     <nav className="navBar">
       <div style={{ marginTop: "10px" }}>
         <Link to="/FE_jemappellenoraPersonal" className="mylinks">
-          <img src={homeIcon} style={{ filter: "contrast(0%)" }} />
+          <img alt="homeIcon" src={homeIcon} style={{ filter: "contrast(0%)" }} />
           Profile
         </Link>
       </div>
       <div style={{ marginTop: "10px" }}>
         <Link to="/experiences" className="mylinks">
-          <img src={computerIcon} style={{ filter: "contrast(0%)" }} />
+          <img alt= "computerIcon" src={computerIcon} style={{ filter: "contrast(0%)" }} />
           Experiences
         </Link>
       </div>
       <div style={{ marginTop: "10px" }}>
         <Link to="/projects/" className="mylinks">
-          <img src={projectIcon} style={{ filter: "contrast(0%)" }} />
+          <img alt="projIcon" src={projectIcon} style={{ filter: "contrast(0%)" }} />
           Projects
         </Link>
       </div>
