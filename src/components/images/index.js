33cd902176ddc001675f79ae3c6ade6a94f7561c
jemappellenora <chingwlau@gmail.com>
2021-06-19 20:10:16 -0400

    styling: add project images for display

--- src/components/images/index.js
+++ src/components/images/index.js
@@ -5,3 +5,5 @@ export { default as homeIcon } from "./homeIcon.png";
 export { default as projectIcon } from "./projectIcon.png";
 export { default as computerIcon } from "./computerIcon.png";
 export { default as happyFaceIcon } from "./happyFaceIcon.png";
+export { default as icompostNyc } from "./icompostNyc.png";
+export { default as ecoCarrot } from "./ecoCarrot.png";
